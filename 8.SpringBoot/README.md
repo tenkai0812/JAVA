1ea7c47d47d7980a75f1d410da14607c9eed7695
Skyler <skylien0966065599@gmail.com>
2024-09-24 17:07:19 +0800

    start up

---
+++ 8.SpringBoot/README.md
@@ -0,0 +1,5 @@
+# Spring Boot 
+## Some personal problems
+* 使用Java Help Center bulid Spring Boot 會有檔名問題(尚未細看)
+* 請直接使用VScode命令選擇區:Spring Initializer
+* 請注意自己電腦port使用情況:Spring Boot default port number: 8080
---
+++ 8.SpringBoot/springboottest/src/main/java/com/example/springboottest/springboottestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springboottestApplication.class, args);
+	}
+
+}
