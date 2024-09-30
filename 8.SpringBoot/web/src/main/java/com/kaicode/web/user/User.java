d8136736d5c45c4f2c3f241dfcee8e61b306f27d
Skyler <skylien0966065599@gmail.com>
2024-09-30 21:58:49 +0800

    rewrite a new version and connect to postgresql

---
+++ 8.SpringBoot/web/src/main/java/com/kaicode/web/user/User.java
@@ -0,0 +1,45 @@
+package com.kaicode.web.user;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@ToString(exclude = "password")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "Created_at")
+    private Date createdAt;
+    
+    @Column(name = "last_login")
+    private Date lastLogin;
+
+    private LocalDate dob;
+
+    // 添加自定義構造函數
+    public User(Long id, String username, String email, Date createdAt, Date lastLogin, LocalDate dob) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.lastLogin = lastLogin;
+        this.dob = dob;
+    }
+}
---
+++ 8.SpringBoot/web/src/main/java/com/kaicode/web/user/UserController.java
@@ -0,0 +1,23 @@
+package com.kaicode.web.user;
+
+import java.util.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping(path = "api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+}
